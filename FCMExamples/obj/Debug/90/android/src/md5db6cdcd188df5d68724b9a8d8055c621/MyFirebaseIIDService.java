3913378af1b39a45965822ac702fdd99cf9ef73d
sidhuraj <mr.prvsantosh@rediffmail.com>
2020-04-06 15:06:29 +0530

    first commit

---
+++ FCMExamples/obj/Debug/90/android/src/md5db6cdcd188df5d68724b9a8d8055c621/MyFirebaseIIDService.java
@@ -0,0 +1,47 @@
+package md5db6cdcd188df5d68724b9a8d8055c621;
+
+
+public class MyFirebaseIIDService
+	extends com.google.firebase.iid.FirebaseInstanceIdService
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onTokenRefresh:()V:GetOnTokenRefreshHandler\n" +
+			"";
+		mono.android.Runtime.register ("FCMExamples.MyFirebaseIIDService, FCMExamples", MyFirebaseIIDService.class, __md_methods);
+	}
+
+
+	public MyFirebaseIIDService ()
+	{
+		super ();
+		if (getClass () == MyFirebaseIIDService.class)
+			mono.android.TypeManager.Activate ("FCMExamples.MyFirebaseIIDService, FCMExamples", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onTokenRefresh ()
+	{
+		n_onTokenRefresh ();
+	}
+
+	private native void n_onTokenRefresh ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ FCMExamples/obj/Debug/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 2ead8ac8a9873e9c768885b0a142be0595bb2d98 */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1513
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 396407
+	.include	"typemap.jm.inc"
